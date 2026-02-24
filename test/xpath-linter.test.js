520e30a5be73720a26c3cd2308b937f2fd79c8eb
alexachenko <alexa.volodchenko@yandex.ru>
2026-02-24 10:45:11 +0300

    interim results

--- test/xpath-linter.test.js
+++ test/xpath-linter.test.js
@@ -15,24 +15,50 @@ const assert = require('assert')
 const PACKS = allFilesFrom(path.resolve(__dirname, 'resources', 'xpath-packs'))
 
 describe('xpath-linter', function() {
-  PACKS.forEach((pack) => {
+  PACKS.forEach((pack) => {//паки с тестами
     const yml = yaml.parsedFromFile(pack)
     const lint = yaml.parsedFromFile(
         path.resolve(__dirname, '../src/resources', `${yml.pack}.yaml`),
-    )
+    )//используем пак, заявленный вначале
+
+    const packs = new Set ([])
     const input = xml.parsedFromString(yml.input)
+
     it(`should find ${yml.found.amount} node(s) by xpath in ${yml.pack}`, function() {
-      const evaluated = evaluate_xpath(input, lint.xpath)
-      assert.equal( //количество исключений, ловимых указанным правилом
-          evaluated.length,
-          yml.found.amount,
-      )
+      let evaluated = evaluate_xpath(input, lint.xpath)
+      let amount = evaluated.length
+
       yml.found.positions.forEach((position, index) => {
-        if (yml.found.positions[index][2]===null) {
+        if (yml.found.positions[index][2]===undefined) {
+          assert.equal(evaluated[index].line, yml.found.positions[index][0])
+          assert.equal(evaluated[index].pos, yml.found.positions[index][1])
+        }
+        else{
+          packs.add(yml.found.positions[index][2])
+        }
+      })
+      packs.forEach((pack) => {
+        const lintForEach = yaml.parsedFromFile(
+            path.resolve(__dirname, '../src/resources', `${pack}.yaml`),
+        )
+        evaluated = evaluate_xpath(input, lintForEach.xpath)
+        yml.found.positions.forEach((position, index) => {
+        if (yml.found.positions[index][2]!==undefined) {
+          assert.equal(evaluated[index].line, yml.found.positions[index][0])
+          assert.equal(evaluated[index].pos, yml.found.positions[index][1])
+        }
+        })
+      })
+      yml.found.positions.forEach((position, index) => {
+        if (yml.found.positions[index][2]==null) {
           assert.equal(evaluated[index].line, yml.found.positions[index][0])
           assert.equal(evaluated[index].pos, yml.found.positions[index][1])
         }
       })
+      assert.equal(
+          evaluated.length,
+          yml.found.amount,
+      )
     })
     it(`should return defects array with ${yml.found.amount} elements`, function() {
       const defects = lint_by_xpath(input)
