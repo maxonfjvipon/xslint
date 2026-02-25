5867532a27b24b82be95b2ed6769f7e19a850def
alexachenko <alexa.volodchenko@yandex.ru>
2026-02-25 17:48:01 +0300

    changed tests

--- test/xpath-linter.test.js
+++ test/xpath-linter.test.js
@@ -18,51 +18,39 @@ describe('xpath-linter', function() {
   PACKS.forEach((pack) => {
     const yml = yaml.parsedFromFile(pack)
     const lint = yaml.parsedFromFile(
-      path.resolve(__dirname, '../src/resources', `${yml.pack}.yaml`),
+        path.resolve(__dirname, '../src/resources', `${yml.pack}.yaml`),
     )
     const input = xml.parsedFromString(yml.input)
-    let anotherViolations = 0;
-    yml.found.positions.forEach((position, index) => {
-      if (!(yml.found.positions[index][2]===null || yml.found.positions[index][2]===undefined)) {
-        anotherViolations++
-      }
-    })
-    it(`should find ${yml.found.amount-anotherViolations} node(s) by xpath in ${yml.pack}`, function() {
+    const other = yml.found.positions.filter((pos) => pos.length == 3).length
+    it(`should find ${yml.found.amount - other} node(s) by xpath in ${yml.pack}`, function() {
       const evaluated = evaluate_xpath(input, lint.xpath)
-
-      yml.found.positions.forEach((position, index) => {
-        assert.equal(
+      assert.equal(
           evaluated.length,
-          yml.found.amount-anotherViolations,
-        )
-        if (yml.found.positions[index][2]===undefined) {
+          yml.found.amount - other,
+      )
+      yml.found.positions.filter((pos) => pos.length == 2).forEach((pos, index) => {
           assert.equal(evaluated[index].line, yml.found.positions[index][0])
           assert.equal(evaluated[index].pos, yml.found.positions[index][1])
-        } else {
-          position++
-        }
       })
     })
     it(`should return defects array with ${yml.found.amount} elements`, function() {
       const defects = lint_by_xpath(input)
       assert.equal(defects.length, yml.found.amount)
       defects.forEach((defect, index) => {
-        if (yml.found.positions[index][2]===undefined) {
+        if (yml.found.positions[index].length == 2) {
           assert.equal(defect.severity, lint.severity)
           assert.equal(defect.message, lint.message)
-          assert.equal(defect.line, yml.found.positions[index][0])
-          assert.equal(defect.pos, yml.found.positions[index][1])
           assert.equal(defect.name, yml.pack)
         } else {
-          const tempLint = yaml.parsedFromFile(
-            path.resolve(__dirname, '../src/resources', `${yml.found.positions[index][2]}.yaml`),
+          const temp = yaml.parsedFromFile(
+              path.resolve(__dirname, '../src/resources', `${yml.found.positions[index][2]}.yaml`),
           )
-          assert.equal(defect.severity, tempLint.severity)
-          assert.equal(defect.message, tempLint.message)
-          assert.equal(defect.line, yml.found.positions[index][0])
-          assert.equal(defect.pos, yml.found.positions[index][1])
+          assert.equal(defect.severity, temp.severity)
+          assert.equal(defect.message, temp.message)
           assert.equal(defect.name, yml.found.positions[index][2])
         }
+        assert.equal(defect.line, yml.found.positions[index][0])
+        assert.equal(defect.pos, yml.found.positions[index][1])
       })
     })
   })
