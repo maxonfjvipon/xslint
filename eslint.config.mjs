c70fc737d58390c35b1e03bc476e338d93126606
alexachenko <alexa.volodchenko@yandex.ru>
2026-03-28 14:42:29 +0300

    added valid-jsdoc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,7 @@ export default defineConfig([{
         "comma-dangle": "off",
         indent: ["error", 2],
         camelcase: "off",
+        "valid-jsdoc": "on",
 
         "max-len": ["error", {
             code: 300,
