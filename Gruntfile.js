d9a58703688118d04a9152afce9f194d0efdc857
maxonfjvipon <mtrunnikov@gmail.com>
2026-04-06 17:37:15 +0300

    timeout

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function(grunt) {
       test: {
         options: {
           files: ['test/**/*.test.js', '!test/resources/**'],
+          timeout: 10_000
         }
       }
     }
